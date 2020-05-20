2748bb6d7336d5ecc6b1b1f9330da1b781828e35
Yin-Chen Huang <krishuang168@Yin-Chende-MacBook.local>
2020-05-20 07:34:02 -0700

    Catalog Cards

--- src/component/Catalog.js
+++ src/component/Catalog.js
@@ -16,18 +16,26 @@ class Catalog extends Component {
                         <Row><Col>May 1, 2020</Col></Row>
                         <hr />
                         <Row>
-                            <Col><RenderCard title="Flowers" 
+                            <Col>
+                                <RenderCard title="Flowers" 
                                 image="https://www.edenbrothers.com/store/media/Seeds-Flowers/resized/SFFOR113-1_medium.jpg"
-                                /></Col>
-                            <Col><RenderCard title="Veggies" 
+                                />
+                            </Col>
+                            <Col>
+                                <RenderCard title="Veggies" 
                                 image="https://www.edenbrothers.com/store/media/Seeds-Vegetables/resized/SVLET120-1_medium.jpg"
-                                /></Col>
-                            <Col><RenderCard title="Herbs" 
+                                />
+                            </Col>
+                            <Col>
+                                <RenderCard title="Herbs" 
                                 image="https://www.edenbrothers.com/store/media/Seeds-Herbs/resized/SHPER111-1_medium.jpg"
-                                /></Col>
-                            <Col><RenderCard title="Trees & Bushes" 
+                                />
+                            </Col>
+                            <Col>
+                                <RenderCard title="Trees & Bushes" 
                                 image="https://d3hne3c382ip58.cloudfront.net/resized/400x400/cherry-blossom-festival-tour-and-trips-400X400_1572932637.JPG"
-                                /></Col>
+                                />
+                            </Col>
                         </Row>
                     </Col>
                 </Row>
