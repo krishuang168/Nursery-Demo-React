b5f7e2a98e6c1d22e60a09075cdf678aeb426cf9
Yin-Chen Kris Huang <krishuang168@Yin-Chende-MacBook.local>
2020-06-13 01:51:42 -0700

    Redux DevTools

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -3,7 +3,10 @@ import { putInBasket } from "./putInBasket-reducer";
 import { updateBasket } from "./updateBasket-reducer";
 
 export const ConfigureStore = () => {
-  const store = createStore(combineReducers({ updateBasket, putInBasket }));
+  const store = createStore(
+    combineReducers({ updateBasket, putInBasket }),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() // Redux Devtools
+  );
 
   return store;
 };
