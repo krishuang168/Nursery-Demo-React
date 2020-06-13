95757d74209cfc4c863259d3e2aea62b875a3e97
Yin-Chen Kris Huang <krishuang168@Yin-Chende-MacBook.local>
2020-06-13 05:33:40 -0700

    Updates

---
+++ precache-manifest.3545ff9e6847eb6ff568b1d76135f22e.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9eaf14ed521102256d58697fe646599",
+    "url": "/Nursery-Demo/index.html"
+  },
+  {
+    "revision": "a0461ad51fe99a90b921",
+    "url": "/Nursery-Demo/static/css/2.cd43873f.chunk.css"
+  },
+  {
+    "revision": "8febd1ce1a5aa99b3ad2",
+    "url": "/Nursery-Demo/static/css/main.38f161a8.chunk.css"
+  },
+  {
+    "revision": "a0461ad51fe99a90b921",
+    "url": "/Nursery-Demo/static/js/2.c8bc6eea.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Nursery-Demo/static/js/2.c8bc6eea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8febd1ce1a5aa99b3ad2",
+    "url": "/Nursery-Demo/static/js/main.63faaa33.chunk.js"
+  },
+  {
+    "revision": "e1dd6cf3f464f6bc2281",
+    "url": "/Nursery-Demo/static/js/runtime-main.5bea734c.js"
+  },
+  {
+    "revision": "acfdc9324415b0ffcd40cced97c9b934",
+    "url": "/Nursery-Demo/static/media/Gisella Demo.acfdc932.ttf"
+  },
+  {
+    "revision": "862c83306ca682ad1bbc1e66410f108e",
+    "url": "/Nursery-Demo/static/media/cheddar jack.862c8330.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Nursery-Demo/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Nursery-Demo/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Nursery-Demo/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Nursery-Demo/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Nursery-Demo/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "5e90f24ab50f46f76a4670201e60b6a7",
+    "url": "/Nursery-Demo/static/media/logo.5e90f24a.svg"
+  },
+  {
+    "revision": "07fff5186b5824a1bca8ae621ca715ae",
+    "url": "/Nursery-Demo/static/media/shoppingBasket.07fff518.svg"
+  },
+  {
+    "revision": "78c6b04ab065b124b69deaf324f95cc7",
+    "url": "/Nursery-Demo/static/media/shoppingBasketWhite.78c6b04a.svg"
+  },
+  {
+    "revision": "4fb2c2eac0830f074cf867065a086881",
+    "url": "/Nursery-Demo/static/media/sideDeco.4fb2c2ea.svg"
+  }
+]);
