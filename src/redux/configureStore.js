cb73a8373fdaabd07eabe19f3cb557bc3b091cc6
Yin-Chen Kris Huang <krishuang168@Yin-Chende-MacBook.local>
2020-06-18 04:16:07 -0700

    Shouldn't Have Been 2 States

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,9 @@
 import { createStore, combineReducers } from "redux";
-import { putInBasket } from "./putInBasket-reducer";
 import { ReduxBasket } from "./ReduxBasket-reducer";
 
 export const ConfigureStore = () => {
   const store = createStore(
-    combineReducers({ ReduxBasket, putInBasket }),
+    combineReducers({ ReduxBasket, }),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() // Redux Devtools
   );
 
