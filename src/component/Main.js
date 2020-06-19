4dd57cb7262d997b0917508088708aaa28dc322a
Yin-Chen Kris Huang <krishuang168@Yin-Chende-MacBook.local>
2020-06-19 00:21:43 -0700

    Clean Up

--- src/component/Main.js
+++ src/component/Main.js
@@ -24,15 +24,6 @@ class Main extends Component {
       return <Home />;
     };
 
-    // const CampsiteWithId = ({match}) => {
-    //     return (
-    //         <CampsiteInfo
-    //             campsite={this.props.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]}
-    //             comments={this.props.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}
-    //         />
-    //     );
-    // };
-
     return (
       <div>
         <Header />
@@ -41,8 +32,6 @@ class Main extends Component {
           <Route path="/home" component={HomePage} />
           <Route exact path="/about_us" component={About} />
           <Route exact path="/contact_us" component={Contact} />
-          {/* <Route exact path='/directory' render={ () => <Directory campsites={this.props.campsites} />} />
-                    <Route path='/directory/:campsiteId' component={CampsiteWithId} /> */}
           <Route exact path="/catalog" component={Catalog} />
 
           {/* Separate Plant Categories */}
